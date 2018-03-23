1377b2c429d9cf0e3c5b17fee5bef33467a105e7
Andre601 <andre424971@gmx.ch>
2018-03-23 22:29:24 +0100

    IntelliJ almost f*cked me...

--- src/main/java/net/Andre601/util/STATIC.java
+++ src/main/java/net/Andre601/util/STATIC.java
@@ -3,7 +3,7 @@ package net.Andre601.util;
 public class STATIC {
 
     //  Version
-    public static final String VERSION = "1.6.8_SNAPSHOT";
+    public static final String VERSION = "1.6.9_SNAPSHOT";
 
     //  Prefix for all commands (">>[command]")
     public static final String PREFIX = ".";
