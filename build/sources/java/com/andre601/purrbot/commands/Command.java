0af0031a8399bc6cd244751c6a209978db706e84
Ryan C. Arrowsmith <ryanarrowsmith01@gmail.com>
2019-05-16 21:45:48 +0100

    Remove the trash

--- build/sources/java/com/andre601/purrbot/commands/Command.java
+++
@@ -1,6 +0,0 @@
-package com.andre601.purrbot.commands;
-
-import com.github.rainestormee.jdacommand.AbstractCommand;
-import net.dv8tion.jda.core.entities.Message;
-
-public interface Command extends AbstractCommand<Message> {}
